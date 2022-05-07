36dbc4a29268353f44b0e825f02fdf3bb92d0828
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-05-08 01:04:17 +0900

    function area

--- start-hello-react/src/App.js
+++ start-hello-react/src/App.js
@@ -1,19 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Fragment} from 'react'; 
+import React, {Component} from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = `리엑트`;
-  let name2 = undefined
+
+const App = () => {
   return (
-    <Fragment>
-
-      {Object.is(`리엑트`,name) ? (<h1>{name}</h1>) : (<h2>아니다</h2>)}
-      {name2 || "잇힝"}
-    </Fragment>
-   
+    <MyComponent></MyComponent>
   );
-  
-}
+};
 
 export default App;
