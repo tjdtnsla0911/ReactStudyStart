881053a04cd87c8a2d172c7f359f0d0daa2002ed
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-09-13 22:52:20 +0900

    컴포넌트 공부중

--- test/src/App.js
+++ test/src/App.js
@@ -1,27 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
+import React, { Component } from 'react';
+import MyComponents1 from './MyComponent';
+/*rcc 를 치면 class형 컴포넌트가 자동완성되고 ,rsc쓰면 function형 컴포넌트가 자동완성된다. */
+/*
+Class형 컴포넌트형과 Function 컴포넌트의 차이점은 클래스형 컴포넌트의 경우 state 기능 및 라이프사이클 기능을
+사용할수있다는 것과 임의 메서드를 정의할수 있다는 것이다.
 
-function App() {
+함수컴포넌트 장점 : class형 컴포넌트보다 선언하기가 훨씬 쉬움, 메모리 자원도 클래스형 컴포넌트 보다 덜 사용함.
+또한 프젝완성해서 빌드 후 배포할때도 function 컴포넌트를 사용하는것이 결과물의 파일 크기가 더작음.
 
-  console.log('APP 시작 !!!!!!!!');
-  const number = 1234
-  const name = `${number} Study`;
-
-  return (
-
-    <Fragment className="App">
-      <header className="App-header">
-
-        <p>React Test</p>
-           {name === `1234김성수` ? (<h1>왓더 뻑</h1>) : (<h2>로카</h2>)}
-            작동해라
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
-
-    </Fragment>
-  );
+function 컴포넌트의 주요 단점은 state 라이플사이클의 API사용이 불가능하다는 점인데, 이건 v16.8이후 Hooks 기능 도입후 해결됨.
+완전히는 똑같이안되지만 조금 다른방식으로 비슷한 작업가능.
+*/
+const a = "Hello!";
+class App extends Component {
+  
+  render() {
+    return (
+          <MyComponents1/>
+    );
+  }
 }
 
 export default App;
---
+++ test/src/MyComponent.js
@@ -0,0 +1,6 @@
+const MyComponentStart = () => {
+    return (<>
+        <h1>나의 새롭고 멋진 컴포넌트</h1>
+    </>);
+}
+export default MyComponentStart;
